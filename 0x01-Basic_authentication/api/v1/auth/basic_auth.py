e4834957cda3d39a2d58fb971ba46b73d8af45c5
amlaksil <silamlakdesye@gmail.com>
2023-11-08 02:24:39 +0300

    Add decode_base64_authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ This module provides `BasicAuth` class, which extends the `Auth` class
 and provides methods for basic authentication.
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -36,3 +37,30 @@ class BasicAuth(Auth):
         if str_a[0] != 'Basic':
             return None
         return str_a[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the given base64-encoded authorization header value.
+
+        Args:
+            base64_authorization_header (str): The base64-encoded authorization
+            header value.
+
+        Returns:
+            str: The decoded value as a UTF-8 string.
+
+        Notes:
+        - Returns None if `base64_authorization_header` is None or not a string
+        - Returns None if `base64_authorization_header` is not a valid Base64
+          string (raises a `binascii.Error`).
+        - Otherwise, returns the decoded value as a UTF-8 string.
+        """
+        if base64_authorization_header is None\
+           or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except base64.binascii.Error:
+            return None
