3b4b138f1e74715914a2aa06f3b37ca84c2ab79f
amlaksil <silamlakdesye@gmail.com>
2023-11-07 18:07:15 +0300

    Validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -42,7 +42,9 @@ class Auth:
         Returns:
             str: The authorization header value.
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, requests=None) -> TypeVar('user'):
         """
