f913b39f84456140f7391a1add2ef7c111dc07ad
amlaksil <silamlakdesye@gmail.com>
2023-11-08 05:38:56 +0300

    Update require_auth to handle * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,8 +27,13 @@ class Auth:
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        path = (path if '/' == path[len(path) - 1] else path + '/')
-        if path in excluded_paths:
+        path_t = (path if '/' == path[len(path) - 1] else path + '/')
+        for path in excluded_paths:
+            if path[len(path) - 1] == '*':
+                sub_path = path[:len(path) - 1]
+                if sub_path in path_t:
+                    return False
+        if path_t in excluded_paths:
             return False
         return True
 
