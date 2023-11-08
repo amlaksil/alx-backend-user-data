a70fb5c21fd9d1f1e48a5e5cbf277c5c7567bf5d
amlaksil <silamlakdesye@gmail.com>
2023-11-08 03:53:19 +0300

    Add user_object_from_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ and provides methods for basic authentication.
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -96,3 +98,32 @@ class BasicAuth(Auth):
             return (None, None)
         user_credentials = decoded_base64_authorization_header.split(':')
         return user_credentials[0], user_credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on the provided email and password.
+
+        Args:
+            user_email (str): The email of the user.
+            user_pwd (str): The password of the user.
+
+        Returns:
+            TypeVar('User'): The User instance if the email and password are
+            valid.
+            None: If any of the following conditions are met:
+                - `user_email` is None or not a string.
+                - `user_pwd` is None or not a string.
+                - The database (file) does n't contain any User instance with
+                  an email equal to `user_email`.
+                - `user_pwd` is not the password of the User instance found.
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
