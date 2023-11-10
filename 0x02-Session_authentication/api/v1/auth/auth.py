2f9246d3a348eb9560f5188e151e4032d1bd60a5
amlaksil <silamlakdesye@gmail.com>
2023-11-10 04:43:11 +0300

    Add session_cookie method

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,13 +4,24 @@ This module provides the `Auth` class, which serves as a template
 for all authentication systems.
 """
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Optional
+import os
 
 
 class Auth:
     """
     Auth class serves as a template for all authentication systems.
     """
+
+    def __init__(self):
+        """
+        Initializes an instance of the AuthHandler class.
+
+        The constructor retrieves the session name from the SESSION_NAME
+        environment variable. If the environment variable is not defined, it
+        assigns a default session name of '_my_session_id'.
+        """
+        self.session_name = os.environ.get('SESSION_NAME', '_my_session_id')
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
@@ -63,3 +74,21 @@ class Auth:
 
         """
         return None
+
+    def session_cookie(
+            self, request: Optional[object] = None) -> Optional[str]:
+        """
+        Returns the value of the session cookie from a given request.
+
+        Args:
+            request (object, optional): The request object containing cookies.
+            Defaults to None.
+
+        Returns:
+            str or None: The value of the session cookie if found, or
+        None if the request is None or the cookie is not present.
+        """
+        if request is None:
+            return None
+
+        return request.cookies.get(self.session_name, None)
