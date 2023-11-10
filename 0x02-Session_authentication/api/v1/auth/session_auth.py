71887100a4500813c41d0cb50ec9acfe60e95fe7
amlaksil <silamlakdesye@gmail.com>
2023-11-10 04:01:00 +0300

    Create create_session method and a class attribute user_id_by_session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ This module contains a SessionAuth class for implementing
 session-based authentication.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -13,4 +14,24 @@ class SessionAuth(Auth):
     This class inherits from the Auth class and serves as the starting point
     for implementing a new authentication mechanism based on sessions.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id.
+
+        Args:
+            user_id (str): The user ID for which to create a session.
+
+        Returns:
+            str: The generated session ID.
+
+        Raises:
+            None.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
