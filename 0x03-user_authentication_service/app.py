ea627d30ee2ef23cc885b461d573b50f8e1f0b70
amlaksil <silamlakdesye@gmail.com>
2023-11-17 04:07:43 +0300

    Implement update_password function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,6 @@
 
 from auth import Auth
 from flask import Flask, jsonify, request, abort, redirect
-from sqlalchemy.orm.exc import NoResultFound
 
 AUTH = Auth()
 
@@ -101,7 +100,37 @@ def get_reset_password_token() -> str:
     try:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
-    except NoResultFound:
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """
+    Endpoint to update the password using a reset token.
+
+    Args:
+        email (str): The email of the user requesting the password update.
+        reset_token (str): The reset token received by the user.
+        new_password (str): The new password to be set.
+
+    Returns:
+        JSON response containing the email and a success
+        message if the password update is successful.
+        HTTP status code 403 (Forbidden) if the password update fails.
+
+    Raises:
+        Exception: If an error occurs during the password update process.
+
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except Exception:
         abort(403)
 
 
