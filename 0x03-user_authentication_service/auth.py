dae5e8447c1211d0c8a3a04a53dce9224c7d216e
amlaksil <silamlakdesye@gmail.com>
2023-11-17 04:30:25 +0300

    Refactor update_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -169,9 +169,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            return self._db.update_user(
-                user.id,
-                hashed_password=_hash_password(password),
-                reset_token=None)
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
         except NoResultFound:
             raise ValueError
